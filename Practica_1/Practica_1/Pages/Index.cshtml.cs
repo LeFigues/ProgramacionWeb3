bd32f607e75c95385834dca191faa1311a5e40c6
LeFigues <figue.bo.mf@gmail.com>
2022-08-09 19:27:51 -0400

    firstCommit

---
+++ Practica_1/Practica_1/Pages/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Practica_1.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
